26b8fb3d97ddf7cb095e2260b2de14df8f2d4bd1
Ocean0000000 <vantocean509@gmail.com>
2021-05-04 19:34:49 -0700

    Added files

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+console.log(outerWidth, outerHeight);
+
+var vw = outerWidth / 100;
+var vh = outerHeight / 100;
+console.log(vw, vh);
+
+var animateText = (document.querySelectorAll(".content"));
+console.log(animateText);
