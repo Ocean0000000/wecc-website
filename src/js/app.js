72b3ed6c5619d17dc8ed97cd4b5fc69b96e6a0c4
Ocean0000000 <vantocean509@gmail.com>
2021-05-07 00:42:45 -0700

    5/7 Update
    made progress on interactive nav burger

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,10 @@ console.log(vw, vh);
 
 var animateText = (document.querySelectorAll(".content"));
 console.log(animateText);
+
+var navBurger = document.querySelector("#navBurger");
+console.log(navBurger);
+
+navBurger.addEventListener("click", {
+    
+});
