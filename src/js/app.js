03a055f21c60ccb9609371ea78773f3c1780af66
Ocean0000000 <vantocean509@gmail.com>
2021-05-11 22:26:36 -0700

    5/11 Update
    completed navbar

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,20 @@
-console.log(outerWidth, outerHeight);
+// console.log(outerWidth, outerHeight);
 
 var vw = outerWidth / 100;
 var vh = outerHeight / 100;
-console.log(vw, vh);
-
-var animateText = (document.querySelectorAll(".content"));
-console.log(animateText);
+// console.log(vw, vh);
 
 var navBurger = document.querySelector("#navBurger");
-console.log(navBurger);
+// console.log(navBurger);
+var navList = document.querySelector(".nav__list");
+// console.log(navList);
 
-navBurger.addEventListener("click", {
-    
-});
+navBurger.addEventListener('click', navToggle);
+
+function navToggle() {
+    navBurger.classList.toggle('navBurgerToggle');
+    navList.classList.toggle('show');
+}
+
+var animateText = (document.querySelectorAll(".content"));
+// console.log(animateText);
