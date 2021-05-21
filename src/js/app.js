78029d284d9cb0b618e88656ce0e19ad72950bd8
Ocean0000000 <vantocean509@gmail.com>
2021-05-20 21:01:37 -0700

    5/20 Update
    Added footer content, removed extra js code, added images, and made minor changes in sass

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,4 @@
-// console.log(outerWidth, outerHeight);
-
-var vw = outerWidth / 100;
-var vh = outerHeight / 100;
-// console.log(vw, vh);
-
 $('#navBurger').on('click', function() {
     $('#navBurger').toggleClass('navBurgerToggle');
     $('#navList').toggleClass('show');
 });
-
-var animateText = $('.title');
-// console.log(animateText)
